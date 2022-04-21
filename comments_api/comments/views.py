285e915fddb091063e58b025a17e300eec8c2a97
gogoshew <iabalymov@gmail.com>
2022-04-21 10:08:21 +0300

    Added viewsets and created urls

--- comments_api/comments/views.py
+++ comments_api/comments/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from comments.models import Comment, CommentAnswer
+from comments.serializers import CommentSerializer, CommentAnswerSerializer
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class AnswersViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = CommentAnswer.objects.all()
+    serializer_class = CommentAnswerSerializer
